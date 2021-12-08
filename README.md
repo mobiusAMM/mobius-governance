2f84b9c5a0489235670493b83d9d3c41fdb200f4
Kyle <kyscott@umich.edu>
2021-12-08 12:39:03 -0500

    queue and execute and readme

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 This governance platform is a fork of Compound Finance's Governor Bravo contracts. To adapt for veMOBI, we call the balanceOfAt() function rather than getPriorVotes(). The initiation has also been removed because this is the first instance of governance for Mobius whereas Governor Bravo was the second governing platform for Compound.
 
 TODO: update the admin of the proxy to itself
+
+## How to use
+rename `.env.example` to .env and enter your own seed phrase
+`yarn` to install all the dependencies
+`yarn hardhat compile --network celo` to compile the contracts
+`yarn hardhat deploy --network celo` to deploy the governance
+`yarn hardhat run scripts/submitProposal.ts --network celo` to submit a proposal
---
+++ scripts/executeProposal.ts
@@ -0,0 +1,31 @@
+import hre from "hardhat"
+import { GovernorBravoDelegate__factory } from "../types"
+
+async function main() {
+  const namedAccounts = await hre.getNamedAccounts();
+  const { deployer } = namedAccounts;
+  if (!deployer) {
+    throw new Error("Deployer not found");
+  }
+  const signer = await hre.ethers.getSigner(deployer);
+  if (!signer) {
+    throw new Error("Deployer signer not found");
+  }
+
+  const { address: governanceAddress } = await hre.deployments.get("GovernorBravoDelegator");
+  const governance = GovernorBravoDelegate__factory.connect(governanceAddress, signer);
+
+  const tx = await governance.execute(1,
+    {
+      gasLimit: 8_500_000, 
+      gasPrice: 0.5 * 10 ** 9
+    });
+    await tx.wait()
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
