c9cb1a2a11296957d8db79a2b74267060d0a45ea
Kyle <kyscott@umich.edu>
2021-12-10 17:59:18 -0500

    passed

--- scripts/executeProposal.ts
+++ scripts/executeProposal.ts
@@ -15,7 +15,7 @@ async function main() {
   const { address: governanceAddress } = await hre.deployments.get("GovernorBravoDelegator");
   const governance = GovernorBravoDelegate__factory.connect(governanceAddress, signer);
 
-  const tx = await governance.execute(2,
+  const tx = await governance.execute(3,
     {
       gasLimit: 8_500_000, 
       gasPrice: 0.5 * 10 ** 9
