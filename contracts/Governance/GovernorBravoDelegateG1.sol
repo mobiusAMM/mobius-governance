11f91aa41ac9ddcc4de4b8c36f7b5b9bfd8904e2
Kyle <kyscott@umich.edu>
2021-12-17 09:50:28 -0500

    proposed parameters for initial deployment

--- contracts/Governance/GovernorBravoDelegateG1.sol
+++ contracts/Governance/GovernorBravoDelegateG1.sol
@@ -9,25 +9,25 @@ contract GovernorBravoDelegate is GovernorBravoDelegateStorageV1, GovernorBravoE
     string public constant name = "Mobius Governance";
 
     /// @notice The minimum setable proposal threshold
-    uint public constant MIN_PROPOSAL_THRESHOLD = 1000000e18; // 1,000,000 veMOBI
+    uint public constant MIN_PROPOSAL_THRESHOLD = 500e18; // 500 veMOBI
 
     /// @notice The maximum setable proposal threshold
-    uint public constant MAX_PROPOSAL_THRESHOLD =  20000000e18; //20,000,000 veMOBI
+    uint public constant MAX_PROPOSAL_THRESHOLD =  10000e18; //10,000 veMOBI
 
     /// @notice The minimum setable voting period
-    uint public constant MIN_VOTING_PERIOD = 20; // About 100 seconds
+    uint public constant MIN_VOTING_PERIOD =  17280; // 17280 blocks, about 1 day
 
     /// @notice The max setable voting period
-    uint public constant MAX_VOTING_PERIOD = 120960; // About 1 week
+    uint public constant MAX_VOTING_PERIOD = 120960; // 120960 blocks, about 1 week
 
     /// @notice The min setable voting delay
     uint public constant MIN_VOTING_DELAY = 1;
 
     /// @notice The max setable voting delay
-    uint public constant MAX_VOTING_DELAY = 120960; // About 1 week
+    uint public constant MAX_VOTING_DELAY = 69120; // 69120 blocks, about 4 days
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    uint public constant quorumVotes =  20000000e18; // 400,000 = 4% of veMOBI
+    uint public constant quorumVotes =  4000000e18; // 4,000,000
 
     /// @notice The maximum number of actions that can be included in a proposal
     uint public constant proposalMaxOperations = 10; // 10 actions
